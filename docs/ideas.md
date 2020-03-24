268cb215ec219f219ea0efc65ca986415079064f
benjihillard <51002483+benjihillard@users.noreply.github.com>
2020-03-23 21:47:01 -0400

    milestone 0 added idea page

---
+++ docs/ideas.md
@@ -0,0 +1,19 @@
+Team Zeta
+
+Highlite
+
+Team Overview
+
+Benjamin Hillard - https://github.com/benjihillard
+Corey Sarkis - https://github.com/csarkis7
+Alden Burgess https://github.com/aldenburgess
+
+
+Innovative Idea
+
+Highlite is a minimalist web tool that aims to assist those who have trouble reading due to Dyslexia, ADHD, and other cognitive disabilities. Highlite takes in a range of input texts from burnt PDFs and Docx files to Epubs and copy+pasted text. The software creates an interactive page where the user can adjust the format of the text to the readability that best suits them. This includes changing the font to dyslexic friendly styles and colors, as well changing backgrounds to optimize text contrast. Highlite is a distraction free page that allows the user to blackout text by paragraph, line and even word by word. Users can toggle off these blackouts at their own rate via keystrokes or on a timer if they are feeling competitive. Highlite ensures maximum readability of documents, even if it has to be read one word at a time.
+
+Important Components
+
+Important components will express for setting up a server. We will need a function to extract the text from text documents. We need a function to parse the text and tag all the words, sentences and paragraphs. We need a function that changes color and style schemes based on user input.
+We need an iterator that is either controlled by keystrokes or a timer. We need a function that controls what text gets blocked out based on where the reader is.
--- highlite.js
+++ highlite.js
@@ -1,21 +1,106 @@
-if (window.File && window.FileReader && window.FileList && window.Blob){
-  console.log("thumbs up emoji");
-}else{
-  alert('The File API is not supported by this browser.');
-}
+//file parsing
+var formidable = require('formidable');
+var fs = require('fs');
+var filereader = require('filereader');
+var pdf = require('pdf-parse');
+var Parser = require("simple-text-parser");
+//string parsing
+var cheerio = require('cheerio');
 
-function handleFilesSelect(evt){
-  evt.stopPropagation();
-  evt.preventDefault();
-  var files = evt.target.files;
-  var reader = new FileReader();
-  reader.onload = function(){
-    console.log(reader.result);
-  }
-  reader.readAsDataURL(files.item(0));
-}
+// global variables
+global.wordString = "Sorry something has gone terribly wrong!";
+global.sentanceString = "Sorry something has gone terribly wrong!";
+global.paragraphString = "Sorry something has gone terribly wrong!";
+
+//server
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/read.html', function (req, res){
+  fs.readFile(__dirname + '/read.html', {encoding: 'utf8'}, function(error, data) {
+    var $ = cheerio.load(data); // load in the HTML into cheerio
+    //$('#' + 'bodytext').html(sentanceString);
+    $('p.bodytext').html(sentanceString);
+    fs.writeFile(__dirname + '/read.html', $.html(),function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+      });
+  });
+  res.sendFile(__dirname + '/read.html');
+});
+
+app.post('/', function (req, res){
+    var filecontent = "";
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+      file.path = __dirname + '/uploads/' + file.name;
+      fs.readFile(file.path, function (err, data){
+        var fileBuffer = data;
+        var filePath = file.path;
+        var fileName = file.name;
+        var fileextension = filereader.getFileExtension(fileName);
+        switch (fileextension) {
+          case '.pdf':
+            wordString = "the erfg";
+            pdf(fileBuffer).then(function(data) {
+              var parser = new Parser();
+              var text = data.text;
+              var sentanceCount = 0;
+
+              parser.addRule(/["’]?[A-Z][^.?!]+((?![.?!][’"]?\s["’]?[A-Z][^.?!]).)+[.?!’"]+/ig, function(tag) {
+                sentanceCount++;
+                return "<span class=" + '"' + "sentance" + sentanceCount.toString() + '"' + ">" + tag.substr(0) + "</span>";
+              });
+              //console.log(parser.render(text));
+              sentanceString = parser.render(text);
+            });
+
+            pdf(fileBuffer).then(function(data) {
+              var parser = new Parser();
+              var text = data.text;
+              var wordCount = 0;
+
+              parser.addRule(/(([\S]+))/ig, function(tag) {
+                wordCount++;
+                return "<span class=" + '"' + "word" + wordCount.toString() + '"' + ">" + tag.substr(0) + "</span>";
+              });
+              //console.log(parser.render(text));
+              wordString = parser.render(text);
+            });
 
 
-var x = document.getElementById("files");
+            pdf(fileBuffer).then(function(data) {
+              var parser = new Parser();
+              var text = data.text;
+              var paraCount = 0;
+              //(.+)((\r?\n.+)*)
+              parser.addRule(/[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*/ig, function(tag) {
+                paraCount++;
+                return "<span class=" + '"' + "para" + paraCount.toString() + '"' + ">" + tag.substr(0) + "</span>";
+              });
+              //console.log(parser.render(text));
+              paragraphString = parser.render(text);
+            });
 
-x.addEventListener('change',handleFilesSelect, false);
+
+            break;
+            default:
+              filecontent = filename;
+            }
+
+      });
+    });
+    console.log(paragraphString);
+    console.log(sentanceString);
+    console.log(wordString);
+    return res.redirect('/read.html');
+});
+
+app.listen(3000);
