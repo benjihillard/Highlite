15377300608017fc88e422da4dcf0a12428a1b07
csarkis7 <corey.sarkis@gmail.com>
2020-04-23 18:13:24 -0400

    Added store settings functionality

--- read/read.js
+++ read/read.js
@@ -2,10 +2,6 @@ let backgroundColor;
 let fontColor;
 let highlightColor;
 
-
-
-
-
 //sidebar toggle
 $("#menu-toggle").click(function(e) {
   e.preventDefault();
@@ -16,3 +12,31 @@ $("#menu-toggle").click(function(e) {
     $("#arrow").attr('src','images/arrow.png');
   }
 });
+
+/* Old sidebar toggle
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+*/
+
+function storeSettings() {
+  let ele = document.getElementsByName("highlight");
+  let highlight = null;
+  for (i = 0; i < ele.length; i++) {
+      if (ele[i].checked) {
+          highlight = ele[i].value;
+      }
+  }
+  let hcolor = document.getElementById("highlight").value;
+  let tcolor = document.getElementById("text").value;
+  let bcolor = document.getElementById("background").value;
+  let fontsize = document.getElementById("fontsize").value;
+  let spacing = document.getElementById("spacing").value;
+  let kerning = document.getElementById("kerning").value;
+}
