a3b4347b1c3ac4c4f9d5891eab2250eab14e7cb5
Benjamin Hillard <bhillard@umass.edu>
2020-05-04 20:02:45 -0400

    vvvv

---
+++ spanify-copy.ts
@@ -0,0 +1,59 @@
+var fs = require('fs');
+var pdf = require('pdf-parse');
+var Parser = require("simple-text-parser");
+
+export class spanify {
+
+  public spanify(filePath){
+    var sentanceString;
+    var wordString;
+    var paragraphString;
+    var strings;
+    fs.readFile(filePath, function (err, data){
+      var fileBuffer = data;
+      pdf(fileBuffer).then(function(data) {
+        var parser = new Parser();
+        var text = data.text;
+        var sentanceCount = 0;
+        parser.addRule(/["’]?[A-Z][^.?!]+((?![.?!][’"]?\s["’]?[A-Z][^.?!]).)+[.?!’"]+/ig, function(tag) {
+          sentanceCount++;
+          return "<span class=" + '"' + "sentance" + sentanceCount.toString() + '"' + ">" + " " + tag.substr(0) + "</span>";
+        });
+        sentanceString = parser.render(text);
+        //console.log("Sentence String In: " + sentanceString);
+      });
+      pdf(fileBuffer).then(function(data) {
+        var parser = new Parser();
+        var text = data.text;
+        var wordCount = 0;
+        parser.addRule(/(([\S]+))/ig, function(tag) {
+          wordCount++;
+          return "<span class=" + '"' + "word" + wordCount.toString() + '"' + ">" + " " + tag.substr(0) + " " + "</span>";
+        });
+        wordString = parser.render(text);
+        //console.log("Word String In: " + wordString);
+      });
+      pdf(fileBuffer).then(function(data) {
+        var parser = new Parser();
+        var text = data.text;
+        var paraCount = 0;
+        parser.addRule(/[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*/ig, function(tag) {
+          paraCount++;
+          return "<span class=" + '"' + "para" + paraCount.toString() + '"' + ">" + tag.substr(0) + "</span>";
+        });
+        paragraphString = parser.render(text);
+        //console.log("Paragraph String In: " + paragraphString);
+      });
+    });
+    setTimeout(function(){
+      strings = {
+        "sentences": sentanceString,
+        "words": wordString,
+        "paragraphs": paragraphString
+      }
+      console.log(strings);
+    },100);
+
+    return strings;
+  }
+}
