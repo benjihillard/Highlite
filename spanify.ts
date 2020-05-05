8528edec835e367540c092471e8f58b183613e9a
csarkis7 <csarkis@umass.edu>
2020-05-04 20:58:56 -0400

    Updated spannify

--- spanify.ts
+++ spanify.ts
@@ -5,13 +5,17 @@ let fs = require('fs');
 export class spanify {
 
   public spanify(filePath){
+    var sentanceString;
+    var wordString;
+    var paragraphString;
+    var strings;
     let dataBuffer = fs.readFileSync(filePath);
     pdf(dataBuffer).then(function(data) {
-      let spannedText = {
+      /*let spannedText = {
         'sentance' : 'string error',
         'word' : 'string error',
         'paragraph' : 'string error',
-      }
+      }*/
       var parser = new Parser();
       var text = data.text;
       var sentanceCount = 0;
@@ -21,20 +25,30 @@ export class spanify {
         sentanceCount++;
         return "<span class=" + '"' + "sentance" + sentanceCount.toString() + '"' + ">" + " " + tag.substr(0) + "</span>";
       });
-      spannedText.sentance = parser.render(text);
+      //spannedText.sentance = parser.render(text);
+      sentanceString = parser.render(text);
       parser.addRule(/(([\S]+))/ig, function(tag) {
         wordCount++;
         return "<span class=" + '"' + "word" + wordCount.toString() + '"' + ">" + " " + tag.substr(0) + " " + "</span>";
       });
-      spannedText.word = parser.render(text);
+      //spannedText.word = parser.render(text);
+      wordString = parser.render(text);
       //(.+)((\r?\n.+)*)
       parser.addRule(/[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*/ig, function(tag) {
         paraCount++;
         return "<span class=" + '"' + "para" + paraCount.toString() + '"' + ">" + tag.substr(0) + "</span>";
       });
-
-      spannedText.paragraph = parser.render(text);
-      fs.writeFile(__dirname + "/uploads/span.json", JSON.stringify(spannedText),(err) => {
+      //spannedText.paragraph = parser.render(text);
+      paragraphString = parser.render(text);
+      setTimeout(function(){
+        strings = {
+          "sentences": sentanceString,
+          "words": wordString,
+          "paragraphs": paragraphString
+        }
+        //console.log(strings);
+      },100);
+      fs.writeFile(__dirname + "/uploads/span.json", JSON.stringify(strings),(err) => {
         if (err) throw err;
         console.log('The file has been saved!');
       });
